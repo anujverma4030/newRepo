4ae858fbaec1a5bb9a2f8d10380f4551c27a3749
anujverma4030 <anujvermaa4030@gmail.com>
2025-06-20 16:27:52 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>hello</Text>
+    <View>
+     <Button >
+    </View>
+  </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    justifyContent:"center",
+    alignItems:"center"
+  }
+});
+
+export default App;
